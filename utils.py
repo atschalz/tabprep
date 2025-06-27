8a5f4a765a6f4aabe571580e85b5465f2e62fa0e
atschalz <andrej.tschalzev@web.de>
2025-06-27 16:34:32 -0700

    Update utils and add function to get benchmark metadata

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from numba import njit
 import pandas as pd
 import numpy as np
@@ -19,6 +20,190 @@ from category_encoders import TargetEncoder
 import lightgbm as lgb
 from statsmodels.stats.multitest import multipletests
 from scipy.special import expit
+from tqdm import tqdm
+
+def _get_n_repeats(n_instances: int) -> int:
+    """
+    Get the number of n_repeats for the full benchmark run based on the 2025 paper.
+    If < 2500 samples, n_repeats = 10, else n_repeats = 3
+
+    Parameters
+    ----------
+    n_instances: int
+
+    Returns
+    -------
+    n_repeats: int
+    """
+    if n_instances < 2500:
+        n_repeats = 10
+    else:
+        n_repeats = 3
+    return n_repeats
+
+def get_metadata_df(tasks, dids, subset: str = None) -> pd.DataFrame:
+    import openml
+    metadata_df = []
+    for task_id, did in tqdm(zip(tasks, dids)):
+        task = openml.tasks.get_task(task_id)
+        dataset = task.get_dataset()
+        repeats, folds, _ = task.get_split_dimensions()
+
+        target_feature = dataset.default_target_attribute
+        is_classification = task.task_type == 'Supervised Classification'
+        ttid = task.task_type
+        n_samples = dataset.qualities["NumberOfInstances"]
+        n_features = dataset.qualities["NumberOfFeatures"]
+        percentage_cat_features = dataset.qualities["PercentageOfSymbolicFeatures"]
+        n_classes = dataset.qualities["NumberOfClasses"]
+
+        if n_classes == 0:
+            problem_type = "regression"
+        else:
+            problem_type = "binary" if n_classes == 2 else "multiclass"
+
+        # From Paper
+        if n_samples < 2_500:
+            tabarena_repeats = 10
+        elif n_samples > 250_000:  # Fallback
+            tabarena_repeats = 1
+        else:
+            tabarena_repeats = 3
+
+        # Bools for constrained methods from paper
+        # - 10k training samples (so 2/3 * n_samples)
+        can_run_tabpfnv2 = (
+            (n_samples <= 15_000) and (n_features <= 500) and (n_classes <= 10)
+        )
+        # - n_classes != 0 as TabICL only works for classification
+        can_run_tabicl = (
+            (n_samples <= 150_000) and (n_features <= 500) and (n_classes != 0)
+        )
+
+        metadata_df.append(
+            [
+                did,
+                task_id,
+                ttid,
+                target_feature,
+                is_classification,
+                dataset.name,
+                problem_type,
+                n_features,
+                n_samples,
+                n_classes if n_classes != 0 else np.nan,
+                percentage_cat_features,
+                folds,
+                repeats,
+                tabarena_repeats,
+                can_run_tabpfnv2,
+                can_run_tabicl,
+                # REF_MAPPING[dataset.name],
+                # SOURCE_MAP[dataset.name],
+                # DOMAIN_MAP[dataset.name],
+                dataset.collection_date,
+                dataset.licence,
+                # URL_OVERWRITE_MAP.get(dataset.name, dataset.original_data_url),
+            ],
+        )
+
+    metadata_df = pd.DataFrame(
+        metadata_df,
+        columns=[
+            "did",
+            "tid",
+            "ttid",
+            "target_feature",
+            "is_classification",
+            "name",
+            "problem_type",
+            "num_features",
+            "num_instances",
+            "num_classes",
+            "percentage_cat_features",
+            "num_folds",
+            "openml_num_repeats",
+            "tabarena_num_repeats",
+            "can_run_tabpfnv2",
+            "can_run_tabicl",
+            # "reference",
+            # "data_source",
+            # "domain",
+            "year",
+            "licence",
+            # "original_data_url",
+        ],
+    )
+
+    metadata_df["n_folds"] = 10
+    metadata_df["n_repeats"] = metadata_df["num_instances"].apply(_get_n_repeats)
+    metadata_df["n_features"] = (metadata_df["num_features"] - 1).astype(int)
+    metadata_df["n_samples_test_per_fold"] = (metadata_df["num_instances"] / metadata_df["n_folds"]).astype(int)
+    metadata_df["n_samples_train_per_fold"] = (metadata_df["num_instances"] - metadata_df["n_samples_test_per_fold"]).astype(int)
+
+    metadata_df["dataset"] = metadata_df["name"]
+
+    if subset is None:
+        pass
+    elif subset == "TabPFNv2":
+        metadata_df = metadata_df[metadata_df["n_samples_train_per_fold"] <= 10000]
+        metadata_df = metadata_df[metadata_df["n_features"] <= 500]
+        metadata_df = metadata_df[metadata_df["NumberOfClasses"] <= 10]
+    elif subset == "TabICL":
+        metadata_df = metadata_df[metadata_df["n_samples_train_per_fold"] <= 100000]
+        metadata_df = metadata_df[metadata_df["n_features"] <= 500]
+        metadata_df = metadata_df[metadata_df["NumberOfClasses"] > 0]
+    else:
+        raise ValueError(f"Unknown subset: {subset}")
+
+    # Merge on Task ID
+    # metadata_df = pd.merge(
+    #     metadata_df, additional_metadata_df, on="task_id", how="left"
+    # )
+    #metadata_df.to_csv("metadata/tabarena_dataset_metadata.csv", index=False)
+    return metadata_df
+
+
+
+
+
+def get_benchmark_dataIDs(benchmark_name):
+    import openml
+    if benchmark_name == "Grinsztajn": # Use the most original version of each unique dataset
+        # collection_id = 334 # Tabular benchmark categorical classification
+        # collection_id = 335 # Tabular benchmark categorical regression
+        # collection_id = 336 # Tabular benchmark numerical regression
+        # collection_id = 337 # Tabular benchmark numerical classification
+        joint_tids = []
+        joint_dids = []
+        unique_names = set()
+        for collection_id in [335, 336,  334, 337]:
+            benchmark_suite = openml.study.get_suite(collection_id)
+            tids = benchmark_suite.tasks
+            dids = benchmark_suite.data
+            for did, tid in zip(dids,tids):
+                task = openml.tasks.get_task(tid) 
+                data = openml.datasets.get_dataset(did) 
+                if data.name not in unique_names:
+                    joint_tids.append(tid)
+                    joint_dids.append(did)
+                    unique_names.add(data.name)
+
+        return joint_tids, joint_dids
+
+    elif benchmark_name == "TabArena":
+        collection_id = 457  # TabArena
+        benchmark_suite = openml.study.get_suite(collection_id)
+        tasks = benchmark_suite.tasks
+        data = benchmark_suite.data
+        return tasks, data
+
+    elif benchmark_name == "TabZilla":
+        collection_id = 379
+        benchmark_suite = openml.study.get_suite(collection_id)
+        tasks = benchmark_suite.tasks
+        data = benchmark_suite.data
+        return tasks, data
 
 def p_value_sign_test_median_greater_than_zero(data):
     """
@@ -2208,11 +2393,14 @@ def analyze_feature_types(X, y, target_type='binary', early_stopping_rounds=20, 
         "significances": significances
     }
 
-def make_cv_scores_with_early_stopping(target_type, scorer, cv, early_stopping_rounds=20, vectorized=False):
+def make_cv_scores_with_early_stopping(target_type, scorer, cv, early_stopping_rounds=20, 
+                                       vectorized=False, verbose=False,
+                                       groups=None
+                                       ):
     """CV creation function for vectorized versions of the TargetEncoderModels."""
     def cv_scores_with_early_stopping(X_df, y_s, pipeline):
         scores = []
-        for train_idx, test_idx in cv.split(X_df, y_s):
+        for train_idx, test_idx in cv.split(X_df, y_s, groups=groups):
             X_tr, y_tr = X_df.iloc[train_idx], y_s.iloc[train_idx]
             X_te, y_te = X_df.iloc[test_idx], y_s.iloc[test_idx]
 
@@ -2224,7 +2412,7 @@ def make_cv_scores_with_early_stopping(target_type, scorer, cv, early_stopping_r
                     X_tr, y_tr,
                     **{
                         "model__eval_set": [(X_te, y_te)],
-                        "model__callbacks": [lgb.early_stopping(early_stopping_rounds, verbose=False)],
+                        "model__callbacks": [lgb.early_stopping(early_stopping_rounds, verbose=verbose)],
                         # "model__verbose": False
                     }
                 )
