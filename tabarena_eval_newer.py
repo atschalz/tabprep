785032775c5c9a0c46ee9595445524ef514ec1c8
atschalz <andrej.tschalzev@web.de>
2025-06-28 11:14:44 -0700

    Update evaluation script

--- tabarena_eval_newer.py
+++ tabarena_eval_newer.py
@@ -14,102 +14,121 @@ from tabrepo.nips2025_utils import load_results
 
 
 if __name__ == '__main__':
-    expname = str(Path(__file__).parent / "experiments" / "current")  # folder location to save all experiment artifacts
-    repo_dir = str(Path(__file__).parent / "repos" / "current")  # Load the repo later via `EvaluationRepository.from_dir(repo_dir)`
+    my_exp_name = "openml"
+    input_format = "openml"
+    benchmark = "TabZilla"
+
+    expname = str(Path(__file__).parent / "experiments" / benchmark / my_exp_name)  # folder location to save all experiment artifacts
+    repo_dir = str(Path(__file__).parent / "repos" / benchmark / my_exp_name)  # Load the repo later via `EvaluationRepository.from_dir(repo_dir)`
     ignore_cache = False  # set to True to overwrite existing caches and re-run experiments from scratch
 
-    task_metadata = load_task_metadata()
+
+    if benchmark == "TabZilla":
+        from utils import get_benchmark_dataIDs, get_metadata_df
+        tids, dids = get_benchmark_dataIDs("TabZilla")  
+        task_metadata = get_metadata_df(tids, dids)    
+    elif benchmark == "Grinsztajn":
+        from utils import get_benchmark_dataIDs, get_metadata_df
+        tids, dids = get_benchmark_dataIDs("Grinsztajn")  
+        task_metadata = get_metadata_df(tids, dids)    
+    elif benchmark == "TabArena":
+        task_metadata = load_task_metadata()
+
 
     # Sample for a quick demo
     # datasets = ["maternal_health_risk", "anneal", "customer_satisfaction_in_airline", "MIC", "airfoil_self_noise", "credit-g", "diabetes"]  # 
-    # datasets = [i for i in list(task_metadata["name"]) if i not in ['superconductivity']]  # Exclude some datasets for a quick demo
-    datasets = ['Bioresponse',
-        'Food_Delivery_Time',
-        'SDSS17',
-        'airfoil_self_noise',
-        'coil2000_insurance_policies',
-        'customer_satisfaction_in_airline',
-        'heloc',
-        'students_dropout_and_academic_success',
-        'superconductivity']
-     
-    # datasets = [#'Food_Delivery_Time', 
-    #     'MIC', 
-    #     'SDSS17', 
-    #     'anneal', 
-    # 'airfoil_self_noise',
-    #    'kddcup09_appetency', 'coil2000_insurance_policies', 'Diabetes130US',
-    #    'heloc', 'E-CommereShippingData', 'in_vehicle_coupon_recommendation',
-    #    'Amazon_employee_access', 'churn', 'Fitness_Club',
-    #    'online_shoppers_intention', 'credit_card_clients_default',
-    #    'HR_Analytics_Job_Change_of_Data_Scientists', 'GiveMeSomeCredit',
-    #    'customer_satisfaction_in_airline', 'Is-this-a-good-customer',
-    #    'qsar-biodeg', 'Bioresponse', 
-    # 'maternal_health_risk',
-    #    'students_dropout_and_academic_success', 
-    #    'superconductivity',
-    # "website_phishing"
-    # ]
+    # datasets = [i for i in list(task_metadata["name"]) if i not in ['lymph', 'audiology', 'albert', 'guillermo']]  # Exclude some datasets for a quick demo
+    datasets = ['vehicle', 'electricity', 'elevators', 'nomao', 'artificial-characters', 'SpeedDating', 'airlines']
+    # datasets = [
+    #     #'analcatdata_supreme', 
+    #              'diamonds', 'Mercedes_Benz_Greener_Manufacturing', #in csv mode
+    #             #  'Brazilian_houses', 
+    #              'Bike_Sharing_Demand', 'nyc-taxi-green-dec-2016', 
+    #              'house_sales', 'particulate-matter-ukair-2017', 'seattlecrime6', 
+    #              'Allstate_Claims_Severity', 'Airlines_DepDelay_1M', 'elevators', 
+    #              #'Ailerons', 
+    #              'electricity', 
+    #              'eye_movements', 'default-of-credit-card-clients', 'road-safety', 
+    #              'bank-marketing', # 'california', 
+    #              'heloc'
+    #              ]
+    # datasets = ['superconductivity']
 
-    folds = [0,1,2]
+    # datasets = ['Amazon_employee_access', 'Diabetes130US',
+    #    'HR_Analytics_Job_Change_of_Data_Scientists', 'Is-this-a-good-customer',
+    #    'MIC', 'SDSS17', 'airfoil_self_noise', 'anneal', 'churn', 'credit-g',
+    #    'credit_card_clients_default', 'customer_satisfaction_in_airline',
+    #    'hiva_agnostic', 'in_vehicle_coupon_recommendation',
+    #    'kddcup09_appetency', 'online_shoppers_intention', 'qsar-biodeg',
+    #    'seismic-bumps', 'students_dropout_and_academic_success']
+
+
+    folds = list(range(1))
+    repeats = None # list(range(3))
 
     # TODO: Need to change the metadata after my stuff
-    new_model_name =  "CatBoost_priordetect"
+    # new_model_name =  "CatBoost_priordetect"
 
-    if new_model_name in ["RealMLP-original", "RealMLP_priordetect"]:
-        from tabrepo.benchmark.models.ag import RealMLPModel as my_model
-    elif new_model_name in ["TabM-original", "TabM_priordetect"]:
-        from tabrepo.benchmark.models.ag import TabMModel as my_model
-    elif new_model_name in ["LightGBM-original", "LightGBM_priordetect"]:
-        from autogluon.tabular.models import LGBModel as my_model
-    elif new_model_name in ["CatBoost-original", "CatBoost_priordetect"]:
-        from autogluon.tabular.models import CatBoostModel as my_model
-    elif new_model_name=="RealMLP":
-      from model_with_cat_detection import RealMLPModelWithCatDetection as my_model
-    elif new_model_name=="TabM":
-        from model_with_cat_detection import TabMModelWithCatDetection as my_model
-    elif new_model_name=="LightGBM":
-       from model_with_cat_detection import LGBModelWithCatDetection as my_model
-    elif new_model_name=="CatBoost":
-       from model_with_cat_detection import CatBoostModelWithCatDetection as my_model
+    results_lst = []  
+    for new_model_name in ["TabM-original", "TabM_priordetect"]:
+    # for new_model_name in ["CatBoost-original", "CatBoost_priordetect"]:
+
+        if new_model_name in ["RealMLP-original", "RealMLP_priordetect"]:
+            from tabrepo.benchmark.models.ag import RealMLPModel as my_model
+        elif new_model_name in ["TabM-original", "TabM_priordetect"]:
+            from tabrepo.benchmark.models.ag import TabMModel as my_model
+        elif new_model_name in ["RF-original", "RF_priordetect"]:
+            from autogluon.tabular.models import RFModel as my_model
+        elif new_model_name in ["XGBoost-original", "XGBoost_priordetect"]:
+            from autogluon.tabular.models import XGBoostModel as my_model
+        elif new_model_name in ["LightGBM-original", "LightGBM_priordetect"]:
+            from autogluon.tabular.models import LGBModel as my_model
+        elif new_model_name in ["CatBoost-original", "CatBoost_priordetect"]:
+            from autogluon.tabular.models import CatBoostModel as my_model
+
+        if "priordetect" in new_model_name:
+            use_ftd = True
+        else:
+            use_ftd = False
+
+        # import your model classes
+        # from autogluon.tabular.models import CatBoostModel as my_model
+        # from tabrepo.benchmark.models.ag import RealMLPModel as my_model
+        # from model_with_cat_detection import RealMLPModelWithCatDetection as my_model
+        # from model_with_cat_detection import LGBModelWithCatDetection as my_model
 
 
-    # import your model classes
-    # from autogluon.tabular.models import CatBoostModel as my_model
-    # from tabrepo.benchmark.models.ag import RealMLPModel as my_model
-    # from model_with_cat_detection import RealMLPModelWithCatDetection as my_model
-    # from model_with_cat_detection import LGBModelWithCatDetection as my_model
+        # This list of methods will be fit sequentially on each task (dataset x fold)
+        methods = [
+            # This will be a `config` in EvaluationRepository, because it computes out-of-fold predictions and thus can be used for post-hoc ensemble.
+            AGModelBagExperiment(  # Wrapper for fitting a single bagged model via AutoGluon
+                # The name you want the config to have
+                name=f"{new_model_name}",
 
+                # The class of the model. Can also be a string if AutoGluon recognizes it, such as `"GBM"`
+                # Supports any model that inherits from `autogluon.core.models.AbstractModel`
+                model_cls=my_model,
+                model_hyperparameters={
+                    # "ag_args_ensemble": {"fold_fitting_strategy": "sequential_local"},  # uncomment to fit folds sequentially, allowing for use of a debugger
+                },  # The non-default model hyperparameters.
+                num_bag_folds=8,  # num_bag_folds=8 was used in the TabArena 2025 paper
+                time_limit=360000,  # time_limit=3600 was used in the TabArena 2025 paper
+                experiment_kwargs={"input_format": input_format},  # input_format="ee" is used to ensure that the feature types are detected correctly
+            ),
+        ]
 
-    # This list of methods will be fit sequentially on each task (dataset x fold)
-    methods = [
-        # This will be a `config` in EvaluationRepository, because it computes out-of-fold predictions and thus can be used for post-hoc ensemble.
-        AGModelBagExperiment(  # Wrapper for fitting a single bagged model via AutoGluon
-            # The name you want the config to have
-            name=f"{new_model_name}_catdetect",
+        exp_batch_runner = ExperimentBatchRunner(expname=expname, task_metadata=task_metadata)
 
-            # The class of the model. Can also be a string if AutoGluon recognizes it, such as `"GBM"`
-            # Supports any model that inherits from `autogluon.core.models.AbstractModel`
-            model_cls=my_model,
-            model_hyperparameters={
-                # "ag_args_ensemble": {"fold_fitting_strategy": "sequential_local"},  # uncomment to fit folds sequentially, allowing for use of a debugger
-            },  # The non-default model hyperparameters.
-            num_bag_folds=8,  # num_bag_folds=8 was used in the TabArena 2025 paper
-            time_limit=360000,  # time_limit=3600 was used in the TabArena 2025 paper
-        ),
-    ]
-
-    exp_batch_runner = ExperimentBatchRunner(expname=expname, task_metadata=task_metadata)
-
-    # Get the run artifacts.
-    # Fits each method on each task (datasets * folds)
-    results_lst: list[dict[str, Any]] = exp_batch_runner.run(
-        datasets=datasets,
-        folds=folds,
-        methods=methods,
-        ignore_cache=ignore_cache,
-        use_ftd=True,
-    )
+        # Get the run artifacts.
+        # Fits each method on each task (datasets * folds)
+        results_lst += exp_batch_runner.run(
+            datasets=datasets,
+            folds=folds,
+            repeats=repeats,
+            methods=methods,
+            ignore_cache=ignore_cache,
+            use_ftd=use_ftd,
+        )
 
     # for res in results_lst:
     #     info = res["method_metadata"]["info"]["children_info"]
@@ -150,21 +169,30 @@ if __name__ == '__main__':
         tabarena_results,
     ], ignore_index=True)
 
+
     res_df = pd.DataFrame({dat: {method: np.mean(metrics.loc[np.logical_and(metrics.dataset==dat, metrics.method==method), "metric_error"].values) for method in metrics.method.unique()} for dat in metrics.dataset.unique()}).transpose()
 
-    if new_model_name=="RealMLP":
-        print(res_df[[f"{new_model_name}_catdetect", "REALMLP (default)",  "AutoGluon 1.3 (4h)",  "CAT (default)"]])
-    elif new_model_name=="TabM":
-        print(res_df[[f"{new_model_name}_catdetect", "TABM (default)",  "AutoGluon 1.3 (4h)",  "CAT (default)"]])
-    elif new_model_name in ["LightGBM", "LightGBM_priordetect"]:
-        print(res_df[[f"{new_model_name}_catdetect", "GBM (default)",  "AutoGluon 1.3 (4h)",  "CAT (default)"]])
-    elif new_model_name=="CatBoost":
-        print(res_df[[f"{new_model_name}_catdetect", "CAT (default)",  "AutoGluon 1.3 (4h)",  "TABM (default)"]])
+    if benchmark == "TabArena":
+        if new_model_name in ["RealMLP", "RealMLP_priordetect", "RealMLP-original", 'CatBoost_priordetect']:
+            print(res_df[[f"{new_model_name}", "REALMLP (default)",  "AutoGluon 1.3 (4h)",  "CAT (default)"]])
+        elif new_model_name in ["TabM", "TabM_priordetect", "TabM-original"]:
+            print(res_df[[f"{new_model_name}", "TABM (default)",  "AutoGluon 1.3 (4h)",  "CAT (default)"]])
+        elif new_model_name in ["LightGBM", "LightGBM_priordetect", "LightGBM-original"]:
+            print(res_df[[f"{new_model_name}", "GBM (default)",  "AutoGluon 1.3 (4h)",  "CAT (default)"]])
+        elif new_model_name in ["CatBoost", "CatBoost_priordetect", "CatBoost-original", "CatBoost_priordetect_catdetect"]:
+            print(res_df[[f"{new_model_name}_catdetect", "CAT (default)",  "AutoGluon 1.3 (4h)",  "TABM (default)"]])
+    else:   
+        print(res_df)
+
 
     with pd.option_context("display.max_rows", None, "display.max_columns", None, "display.width", 1000):
         print(f"Results:\n{metrics.head(100)}")
 
-    calibration_framework = "RF (default)"
+
+    if benchmark == "TabArena":
+        calibration_framework = "RF-original"
+    else: 
+        calibration_framework = "TabM-original" # "RF (default)"
 
     from tabrepo.tabarena.tabarena import TabArena
     tabarena = TabArena(
@@ -184,7 +212,7 @@ if __name__ == '__main__':
 
     leaderboard = tabarena.leaderboard(
         data=metrics,
-        include_elo=True,
+        include_elo=True if benchmark == "TabArena" else False,
         elo_kwargs={
             "calibration_framework": calibration_framework,
         },
