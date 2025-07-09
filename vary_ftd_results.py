d0c4b2a6a9aba2a29c7b3940dbb4a9423b58297f
atschalz <andrej.tschalzev@web.de>
2025-07-09 15:09:06 -0700

    Update ftd variation function

--- vary_ftd_results.py
+++ vary_ftd_results.py
@@ -1,23 +1,16 @@
 import openml
 import pandas as pd
 import numpy as np
-
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from traitlets import default
-from utils import *
-from matplotlib.backends.backend_pdf import PdfPages
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
-from sklearn.impute import SimpleImputer
+from utils import TargetMeanRegressor, UnivariateLinearRegressor, UnivariateLogisticClassifier, get_benchmark_dataIDs
 import pickle
 import os
 import pickle
 import openml
 from ft_detection import FeatureTypeDetector
 from autogluon.features.generators import AutoMLPipelineFeatureGenerator
+import time
 
-def get_config_result(did, tid, dataset_variants: list=['openml'], ftd_params: dict={}):
+def get_config_result(did, tid, dataset_variants: list=['openml'], ftd_params: dict={}, subsample=None):
     res = {}
     
     # Get dataset
@@ -26,6 +19,13 @@ def get_config_result(did, tid, dataset_variants: list=['openml'], ftd_params: d
     y = X[data.default_target_attribute]
     X = X.drop(columns=[data.default_target_attribute])
     
+    if subsample is not None:
+        if X.shape[0] > subsample:
+            X = X.sample(n=subsample, random_state=42)
+            y = y.loc[X.index]
+            X = X.reset_index(drop=True)
+            y = y.reset_index(drop=True)
+
     # Get task type
     task_type = openml.tasks.get_task(tid).task_type
     if task_type == "Supervised Classification" and y.nunique() == 2:
@@ -44,10 +44,12 @@ def get_config_result(did, tid, dataset_variants: list=['openml'], ftd_params: d
 
     if 'openml' in dataset_variants:    
         res['openml'] = {}
-        
+        start_time = time.time()
         ftd = FeatureTypeDetector(target_type=target_type, **ftd_params)
         ftd.fit(X, y, verbose=False)
-        
+        end_time = time.time()
+
+        res['openml']['time'] = end_time - start_time
         res['openml']['new_categorical'] = list(ftd.cat_dtype_maps.keys())
         res['openml']['new_numeric'] = [col for col in X.columns if ftd.dtypes[col]=="numeric" and ftd.orig_dtypes[col]!="numeric"]
         res['openml']['dtypes'] = ftd.dtypes
@@ -56,7 +58,6 @@ def get_config_result(did, tid, dataset_variants: list=['openml'], ftd_params: d
         res['openml']['significances'] = ftd.significances
         res['openml']['params'] = ftd.get_params()
 
-
     if 'AG' in dataset_variants:
         feature_generator = AutoMLPipelineFeatureGenerator()
         feature_generator.fit(X)
@@ -64,10 +65,12 @@ def get_config_result(did, tid, dataset_variants: list=['openml'], ftd_params: d
 
         if X_ag.shape != X.shape or any(X.dtypes.sort_index() != X_ag.dtypes.sort_index()):
             res['AG'] = {}
-            
+            start_time = time.time()
             ftd = FeatureTypeDetector(target_type=target_type, **ftd_params)
             ftd.fit(X_ag, y, verbose=False)
-            
+            end_time = time.time()
+
+            res['AG']['time'] = end_time - start_time
             res['AG']['new_categorical'] = list(ftd.cat_dtype_maps.keys())
             res['AG']['new_numeric'] = [col for col in X_ag.columns if ftd.dtypes[col]=="numeric" and ftd.orig_dtypes[col]!="numeric"]
             res['AG']['dtypes'] = ftd.dtypes
@@ -86,10 +89,12 @@ def get_config_result(did, tid, dataset_variants: list=['openml'], ftd_params: d
 
         if any(X_csv!=X):
             res['csv'] = {}
-            
+            start_time = time.time()
             ftd = FeatureTypeDetector(target_type=target_type, **ftd_params)
             ftd.fit(X_csv, y, verbose=False)
+            end_time = time.time()
 
+            res['csv']['time'] = end_time - start_time
             res['csv']['new_categorical'] = list(ftd.cat_dtype_maps.keys())
             res['csv']['new_numeric'] = [col for col in X_csv.columns if ftd.dtypes[col]=="numeric" and ftd.orig_dtypes[col]!="numeric"]
             res['csv']['dtypes'] = ftd.dtypes
@@ -106,15 +111,46 @@ def get_config_result(did, tid, dataset_variants: list=['openml'], ftd_params: d
     return res
 
 if __name__ == "__main__":
-    exp_name = "ftd_variants"
+    benchmark = "TabZilla"
+    if benchmark == "Grinsztajn":
+        subsample = 10000
+    else:
+        subsample = None
+    
+    exp_name = f"ftd_variants_{benchmark}"
     dataset_variants = ['openml', 'AG', 'csv']
     ftd_params_lst = [
-        ('default', {}),
-        ('cardinality10', {'min_q_as_num': 11}),
-        ('10folds', {'n_folds': 10}),
-        ('default_lgb', {"lgb_model_type": 'default'}),
-        ('high_corr_1', {"use_highest_corr_feature": True, 'num_corr_feats_use': 1}),
-        ('high_corr_5', {"use_highest_corr_feature": True, 'num_corr_feats_use': 5}),
+        ('with_interaction_10', {
+            'verbose': False, 
+            'interpolation_criterion': 'match',
+            'combination_test_min_bins': 2,
+            'combination_test_max_bins': 4096,
+            'alpha': 0.05,
+            'interaction_mode': 10, # ['high_corr', 'all' int]
+            'use_interaction_test': True,
+            'use_leave_one_out_test': True,
+                                      }),
+        ('with_interaction', {
+            'verbose': False, 
+            'interpolation_criterion': 'match',
+            'combination_test_min_bins': 2,
+            'combination_test_max_bins': 4096,
+            'alpha': 0.05,
+            'interaction_mode': 'high_corr', # ['high_corr', 'all' int]
+            'use_interaction_test': True,
+            'use_leave_one_out_test': True,
+                                      }),
+        # ('match_interpol_criterion_lowcard', {'verbose': False, 'interpolation_criterion': 'match', 'combination_test_min_bins': 2, 'min_q_as_num': 3}),
+        # ('match_interpol_criterion', {'verbose': False, 'interpolation_criterion': 'match'}),
+        # ('with_performance_test', {'verbose': False, 'interpolation_criterion': 'match', 'use_performance_test': True, 'lgb_model_type': 'huge-capacity'}),
+        # ('default', {}),
+        # ('full_capacity', {'lgb_model_type': 'full-capacity', 'fit_cat_models': False}),
+        # ('full_capacity_both', {'lgb_model_type': 'full-capacity', 'fit_cat_models': True}),
+        # ('cardinality10', {'min_q_as_num': 11}),
+        # ('10folds', {'n_folds': 10}),
+        # ('default_lgb', {"lgb_model_type": 'default'}),
+        # ('high_corr_1', {"use_highest_corr_feature": True, 'num_corr_feats_use': 1}),
+        # ('high_corr_5', {"use_highest_corr_feature": True, 'num_corr_feats_use': 5}),
     ]
 
     if os.path.exists(f"{exp_name}.pkl"):
@@ -122,22 +158,16 @@ if __name__ == "__main__":
             results = pickle.load(f)
     else:
         results = {}
-    collection_id = 457  # TabArena
-    # collection_id = 334 # Tabular benchmark categorical classification
-    # collection_id = 335 # Tabular benchmark categorical regression
-    # collection_id = 336 # Tabular benchmark numerical regression
-    # collection_id = 337 # Tabular benchmark numerical classification
-    # collection_id = 379 # TabZilla-hard
-    # Fetch the benchmark suite
-    benchmark_suite = openml.study.get_suite(collection_id)
-    dids = benchmark_suite.data
-    tids = benchmark_suite.tasks
+    
 
+    tids, dids = get_benchmark_dataIDs(benchmark)
     for d_num, (did, tid) in enumerate(zip(dids, tids)):
         data = openml.datasets.get_dataset(did)
+        # if data.name !='qsar-biodeg':
+        #     continue
         dataset_name = data.name
-        if dataset_name in ['superconductivity']:
-            continue
+        # if dataset_name in ['guillermo']:
+        #     continue
         if dataset_name not in results:
             results[dataset_name] = {}
         print(dataset_name)
@@ -145,7 +175,7 @@ if __name__ == "__main__":
             ftd_params_name, ftd_params = ftd_params_tuple
             if ftd_params_name not in results[dataset_name]:
                 print(f"Running {ftd_params_name} for {dataset_name}...")
-                res = get_config_result(did=did, tid=tid, dataset_variants=dataset_variants, ftd_params=ftd_params)
+                res = get_config_result(did=did, tid=tid, dataset_variants=dataset_variants, ftd_params=ftd_params, subsample=subsample)
                 results[dataset_name][ftd_params_name] = res
             else:
                 print(f"Skipping {ftd_params_name} for {dataset_name} as it already exists.")
@@ -164,4 +194,5 @@ if __name__ == "__main__":
 
         with open(f"{exp_name}.pkl", "wb") as f:
             pickle.dump(results, f)
+
 
